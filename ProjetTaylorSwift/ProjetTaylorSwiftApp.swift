af82edb5907ff07e9a0f9bbabb56cb05f2b0e5b9
goldorak <goldorak@B15-20.local>
2022-01-31 16:48:19 +0100

    new commit

---
+++ ProjetTaylorSwift/ProjetTaylorSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetTaylorSwiftApp.swift
+//  ProjetTaylorSwift
+//
+//  Created by goldorak on 31/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct ProjetTaylorSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
